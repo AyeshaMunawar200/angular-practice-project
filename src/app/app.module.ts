2aa9f2919c87d4a9d0fedf43756bf44e2c508780
Ayesha Munawar <ayesha.munawar1@gomotive.com>
2023-12-06 20:33:51 +0500

    create a sample component test it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,21 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServerComponent } from 'src/server/server.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
